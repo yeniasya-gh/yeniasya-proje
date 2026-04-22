fb5dc5a9700a560bffe548406d6ccec5c993164d
Aykut Beyaz <ayktbyz@gmail.com>
2026-04-22 10:36:58 +0300

    Refresh web dist with store badges

--- web_dist/flutter_bootstrap.js
+++ web_dist/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3601726151"
+    serviceWorkerVersion: "1309133701"
   }
 });
